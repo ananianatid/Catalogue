06e57fe69945e9fde32956473d519d2e535c3adb
anatide <ananijoellanatide@gmail.com>
2025-07-06 15:48:24 +0000

    mod Product  ressource

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -7,55 +7,95 @@ use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-
+use Illuminate\Support\Str;
+use Filament\Forms\Components\FileUpload;
 class ProductResource extends Resource
 {
     protected static ?string $model = Product::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-computer-desktop';
+    protected static ?string $label = 'Produits';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
+                Forms\Components\Section::make()->schema([
                 Forms\Components\TextInput::make('name')
-                    ->required(),
+                    ->label('Nom')
+                    ->required()
+                    ->reactive() // rend le champ réactif
+                    ->afterStateUpdated(function ($state, callable $set) {
+                        // Met à jour le slug dès que "name" change
+                        $set('slug', Str::slug($state));
+                    }),
                 Forms\Components\TextInput::make('slug'),
                 Forms\Components\TextInput::make('price')
+                    ->label("Prix")
                     ->required()
                     ->numeric()
-                    ->prefix('$'),
+                    ->prefix('FCFA'),
                 Forms\Components\TextInput::make('stock')
                     ->required()
                     ->numeric()
                     ->default(0),
                 Forms\Components\TextInput::make('discount_percent')
+                    ->label("Pourcentage de promotion")
                     ->required()
                     ->numeric()
                     ->default(0),
-                Forms\Components\TextInput::make('category_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('brand_id')
-                    ->required()
-                    ->numeric(),
+                ])->columns(2),
                 Forms\Components\Toggle::make('is_active')
+                    ->label("En vente")
                     ->required(),
                 Forms\Components\Toggle::make('is_featured')
+                    ->label("En Promotion")
                     ->required(),
+                Forms\Components\Section::make()->schema([
+                Forms\Components\Select::make('brand_id')
+                    ->label('Marque')
+                    ->relationship(name: 'brand', titleAttribute: 'name')
+                    ->live()
+                    ->required()
+                    ->preload()
+                    ->afterStateUpdated(fn(Set $set) => $set('state_id', null))
+                    ->searchable(),
+                Forms\Components\Select::make('category_id')
+                    ->label('Catégorie' )
+                    ->relationship(name: 'category', titleAttribute: 'name')
+                    ->live()
+                    ->required()
+                    ->preload()
+                    ->afterStateUpdated(fn(Set $set) => $set('state_id', null))
+                    ->searchable(),
+                ])
+                ->columns(2),
                 Forms\Components\TextInput::make('visibility')
+                    ->default("public")
+                    ->hidden()
                     ->required(),
                 Forms\Components\Textarea::make('description')
                     ->columnSpanFull(),
                 Forms\Components\Textarea::make('tech_sheet')
                     ->columnSpanFull(),
-                Forms\Components\Textarea::make('images')
-                    ->columnSpanFull(),
+                // Forms\Components\Textarea::make('images')
+                //     ->columnSpanFull(),
+                Forms\Components\FileUpload::make('images')
+                    ->label('Images')
+                    ->disk('public') // stockage local (dans storage/app/public)
+                    ->directory('uploads/images') // sous-dossier
+                    ->multiple() // ← cette ligne permet l’upload de plusieurs fichiers
+                    ->image() // accepte uniquement les images
+                    ->reorderable() // possibilité de réorganiser les images
+                    ->preserveFilenames() // facultatif : garde les noms originaux
+                    ->maxSize(2048), // max 2 Mo par image
+
             ]);
     }
 
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $guarded;
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    protected $casts = [
+        'images' => 'json', // ou 'array' fonctionne aussi
+    ];
+
 }
