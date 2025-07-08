646eae4dc71f4a9d3d08117b9982a7ee36f522f8
anatide <ananijoellanatide@gmail.com>
2025-07-08 21:32:08 +0000

    add loading icon

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -15,6 +15,7 @@
         }
     </style>
 <body class="flex flex-col bg-white">
+
     <nav>
         <div class="navbar bg-slate-100 shadow-sm">
             <div class="navbar-start">
@@ -51,8 +52,13 @@
             </div>
         </div>
     </nav>
+    <!-- Spinner visible au départ -->
+    <div id="loading" class="fixed inset-0 flex items-center justify-center bg-white z-50">
+        <span class="loading loading-spinner loading-xl"></span>
+    </div>
 
-    <div class="container lg:w-3/4 mx-auto ">
+
+    <div class="container lg:w-3/4 mx-auto " id="contents" style="display:none;">
         @yield('content')
     </div>
 
@@ -61,7 +67,7 @@
             <div class="sm:flex sm:items-center sm:justify-between">
                 <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                     </svg>
 
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Be market</span>
@@ -85,5 +91,12 @@
             <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://flowbite.com/" class="hover:underline">Be market</a>. All Rights Reserved.</span>
         </div>
     </footer>
+                    <script>
+                        // Quand la page est complètement chargée
+                        window.addEventListener('load', function() {
+                            document.getElementById('loading').style.display = 'none'; // cacher le spinner
+                            document.getElementById('contents').style.display = 'block'; // afficher le contenu
+                        });
+                    </script>
 </body>
 </html>
