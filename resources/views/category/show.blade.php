132f953513a7041e8c2361b47f4cc87424721b31
anatide <ananijoellanatide@gmail.com>
2025-07-08 15:15:28 +0000

    category.show ajour de la card product

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -2,16 +2,6 @@
 @section('title', 'Be market ')
 @section('content')
     @foreach ($products as $product )
-        <div class="m-1 p-1 border-2">
-        <img
-            class="object-contain border-2 border-gray-300 rounded-md h-48 w-40 shadow-sm"
-            src="{{ $product->image[0] ?? 'https://via.placeholder.com/160x192?text=No+Image' }}"
-            alt="{{ $product->name }}"
-        >
-            <p>
-                {{ $product->name }}
-            </p>
-            <a href="{{ route('show.product',['product'=>$product]) }}">voir</a>
-        </div>
+        @include('shared.card', ['product' => $product])
     @endforeach
 @endsection
