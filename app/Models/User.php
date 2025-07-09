489716cf9e778e4b74aeae23214c9ba7738eec0b
anatide <ananijoellanatide@gmail.com>
2025-07-09 15:07:07 +0000

    user model adaptation for filament for error 500

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,45 +2,27 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Models\Contracts\HasName;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements FilamentUser,HasName
+class User extends Authenticatable implements FilamentUser, HasName
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -48,11 +30,13 @@ class User extends Authenticatable implements FilamentUser,HasName
             'password' => 'hashed',
         ];
     }
+
     public function canAccessPanel(\Filament\Panel $panel): bool
     {
-        return $this->active; // or implement your own logic to check access
+        return true; // ou selon ta logique d'accès
     }
-    public function getName(): string
+
+    public function getFilamentName(): string
     {
         return $this->name;
     }
